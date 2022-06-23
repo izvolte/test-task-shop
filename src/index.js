1cae53d80b76de96550d7049e51d52b1bd8482bb
mshepelev <mshepelev@gid.ru>
2022-06-23 20:20:31 +0300

    added styles && craco

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '@/assets/fonts/fonts.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
